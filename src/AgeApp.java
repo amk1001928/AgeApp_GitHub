81d6152f3de975353e24ec0b90f58d54d6346908
Your Name <your.email@example.com>
2024-10-09 11:20:04 +0300

    What can do depending ur age - Task1

--- src/AgeApp.java
+++ src/AgeApp.java
@@ -1,6 +1,15 @@
 public class AgeApp {
     public static void main(String[] args) throws Exception {
+    
+        int age = 20;
         
+        if (age > 0 && age < 18) {
+            System.out.println("You are underage");
+        }
+        else {
+            System.out.println("You are an adult");
+        }
+        System.out.println("Press space to exit");
 
 }
 }
