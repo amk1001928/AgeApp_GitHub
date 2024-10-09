434000916ecbb112c946a0f1163c9ac5b3226619
Your Name <your.email@example.com>
2024-10-09 11:32:03 +0300

    Amended mope drive age - Task3

--- src/AgeApp.java
+++ src/AgeApp.java
@@ -1,13 +1,13 @@
 public class AgeApp {
     public static void main(String[] args) throws Exception {
     
-        int age = 0;
+        int age = 16;
              
         if (age >= 0 && age < 14) {
             System.out.println("You are underage");
         }
 
-        if (age >= 15) {
+        else if (age >= 15) {
             System.out.println("You can drive a moped");
         }
         else if (age >= 65) {
