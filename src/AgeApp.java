e9f158d682562d6e40b501ad496e488f6f496bed
Your Name <your.email@example.com>
2024-10-09 11:25:19 +0300

    Insert Adult age limit - Task2

--- src/AgeApp.java
+++ src/AgeApp.java
@@ -1,15 +1,17 @@
 public class AgeApp {
     public static void main(String[] args) throws Exception {
     
-        int age = 20;
+        int age = 0;
         
-        if (age > 0 && age < 18) {
+        if (age >= 0 && age < 18) {
             System.out.println("You are underage");
         }
-        else {
-            System.out.println("You are an adult");
+        else if (age >= 65) {
+            System.out.println("You are an retired");
         }
-        System.out.println("Press space to exit");
+        else {
+        System.out.println("You are an adult");  
+        }
 
 }
 }
